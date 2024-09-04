0c872b85d7aabf3a4ce94e36246cd7a52b549353
Misha <vechnuignor@mail.ru>
2024-09-04 22:09:47 +0300

    newFile

--- test_folder/tests/main.spec.ts
+++ test_folder/tests/main.spec.ts
@@ -1,11 +1,16 @@
-import { Cell } from "ton-core";
+import { Cell, Address  } from "ton-core";
 import { hex } from "../build/main.compiled.json";
-
+import { Blockchain } from "@ton-community/sandbox";
+import { MainContract } from "../wrappers/MainContract";
 
 describe("test tests", () => {
 	it("test of test", async() => {
 		const codeCell = Cell.fromBoc(Buffer.from(hex,"hex"))[0];
 
+		const blockchain = await Blockchain.create();
 
+		const myContract = blockchain.openContract(
+			await MainContract.createFromConfig({}, codeCell)
+		);
 	});
 });
