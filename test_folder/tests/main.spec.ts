87837a34d4f1872ba7a4a86e3874304953b804b9
Misha <vechnuignor@mail.ru>
2024-09-04 22:03:51 +0300

    receivingASell

--- test_folder/tests/main.spec.ts
+++ test_folder/tests/main.spec.ts
@@ -1,6 +1,11 @@
 import { Cell } from "ton-core";
 import { hex } from "../build/main.compiled.json";
 
+
 describe("test tests", () => {
-	it("test of test", async() => {});
+	it("test of test", async() => {
+		const codeCell = Cell.fromBoc(Buffer.from(hex,"hex"))[0];
+
+
+	});
 });
