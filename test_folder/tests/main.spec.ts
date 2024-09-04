ec3d387b5bd7ad0d9b23f652c06a7fce9921d0c5
Misha <vechnuignor@mail.ru>
2024-09-04 22:14:09 +0300

    addUpdate

--- test_folder/tests/main.spec.ts
+++ test_folder/tests/main.spec.ts
@@ -1,7 +1,8 @@
-import { Cell, Address  } from "ton-core";
+import { Cell, Address, toNano } from "ton-core";
 import { hex } from "../build/main.compiled.json";
 import { Blockchain } from "@ton-community/sandbox";
 import { MainContract } from "../wrappers/MainContract";
+import { send } from "process";
 
 describe("test tests", () => {
 	it("test of test", async() => {
@@ -12,5 +13,9 @@ describe("test tests", () => {
 		const myContract = blockchain.openContract(
 			await MainContract.createFromConfig({}, codeCell)
 		);
+
+		const senderWallet = await blockchain.treasury("sender");
+
+		myContract.sendInternalMessage(senderWallet.getSender(),toNano("0.05"));
 	});
 });
