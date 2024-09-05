5e2545f8c5cb6d14c6e77f5814464e6c7318f420
Misha <vechnuignor@mail.ru>
2024-09-05 09:30:02 +0300

    finishCode

--- test_folder/scripts/deploy.ts
+++ test_folder/scripts/deploy.ts
@@ -1,5 +1,7 @@
 import { Cell, StateInit, beginCell, contractAddress, storeStateInit, toNano } from "ton-core";
 import { hex } from "../build/main.compiled.json";
+import qs from "qs";
+import qrcode from "qrcode-terminal";
 
 async function deployContract() {
 	const codeCell = Cell.fromBoc(Buffer.from(hex,"hex"))[0];
@@ -19,6 +21,31 @@ async function deployContract() {
 		data: dataCell,
 	});
 
-}
+
+	let deployLink =
+		'https://app.tonkeeper.com/transfer/' +
+		address.toString({
+			testOnly: true,
+		}) +
+		"?" +
+		qs.stringify({
+			text: "Deploy contract by QR",
+			amount: toNano("0.1").toString(10),
+			init: stateInitCell.toBoc({idx: false}).toString("base64"),
+		});
+
+	qrcode.generate(deployLink, {small: true }, (qr) => {
+		console.log(qr);
+	});
+
+	let scanAddr = 
+		'https://testnet.tonscan.org/address/' +
+		address.toString({
+			testOnly: true,
+		})
+
+	console.log(scanAddr);
+
+	}
 
 deployContract()
