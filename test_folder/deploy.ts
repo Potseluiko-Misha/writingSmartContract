996744077789b44d30c44b486a09f42e76ff36e5
Misha <vechnuignor@mail.ru>
2024-09-05 09:25:27 +0300

    readyToDeploy

--- test_folder/deploy.ts
+++ test_folder/deploy.ts
@@ -1,4 +1,14 @@
+import { Cell, StateInit, beginCell, contractAddress, storeStateInit, toNano } from "ton-core";
+import { hex } from "../build/main.compiled.json";
+
 async function deployContract() {
+	const codeCell = Cell.fromBoc(Buffer.from(hex,"hex"))[0];
+	const dataCell = new Cell();   
+
+	const stateInit: StateInit = {
+		code: codeCell,
+		data: dataCell,
+	};
 }
 
 deployContract()
